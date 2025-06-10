473d7b11e9f25885f449766b65f302050588e2c0
SerYanez <sergioyanez@outlook.com.ar>
2025-06-09 21:25:49 -0300

    Correcciones

--- practica06-09/ejercicio5.py
+++ practica06-09/ejercicio5.py
@@ -1,13 +1,13 @@
 # Crear una función que convierta grados Celsius a Fahrenheit. Controlar que el
 # valor ingresado sea un número decimal válido.
 
-def convertir_a_f():
-    while True:
-        try:
-            temp = float(input("Ingrese una temperatura en celsius: "))
-            farenheit = temp * 9/5 + 32
-            return farenheit
-        except ValueError:
-            return "Número inválido"
+def convertir_a_f(temp):
+    try:
+        temp = float(temp)
+        farenheit = temp * 9/5 + 32
+        return farenheit
+    except ValueError:
+        return "Número inválido"
 
-print(convertir_a_f())
+celsius = input("Ingrese una temperatura en celsius: ")
+print(convertir_a_f(celsius))
