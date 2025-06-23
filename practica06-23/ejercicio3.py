5dc3d391f826886b8924b54bb8262c1fda3d1a14
SerYanez <sergioyanez@outlook.com.ar>
2025-06-23 19:52:00 -0300

    Práctica de manejo de archivos

--- practica06-23/ejercicio3.py
+++ practica06-23/ejercicio3.py
@@ -1,2 +1,5 @@
 # Permitir el ingreso de nuevas frases por parte del usuario y agregarlas al final de
 # un archivo existente llamado frases.txt, sin borrar su contenido previo.
+
+with open("frases.txt", "a") as archivo:
+    archivo.write(input("Ingrese una nueva frase: "))
--- practica06-23/ejercicio4.py
+++ practica06-23/ejercicio4.py
@@ -2,3 +2,19 @@
 # Reemplazar los puntos decimales por comas en los valores numéricos, y guardar
 # el resultado en un nuevo archivo llamado mediciones_modificadas.txt. Verificar
 # previamente si el archivo existe.
+
+def modif(file, nuevo_archivo):
+    with open(file, "r") as archivo:
+        lineas = archivo.readlines()
+    with open(nuevo_archivo, "w") as nuevo:
+        for i in lineas:
+            linea = i.replace(".", ",")
+            nuevo.write(linea)
+
+with open("mediciones.txt", "r") as archivo:
+    print(archivo.read())
+
+modif("mediciones.txt", "mediciones_modificadas.txt")
+
+with open("mediciones_modificadas.txt", "r") as archivo:
+    print(archivo.read())
