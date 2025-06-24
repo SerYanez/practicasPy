98c1bb6aeb58e002911cde83a03215e0c1193cb4
SerYanez <sergioyanez@outlook.com.ar>
2025-06-24 09:25:10 -0300

    Práctica de manejo de archivos

--- practica06-23/ejercicio11.py
+++ practica06-23/ejercicio11.py
@@ -2,3 +2,19 @@
 # [REMITENTE], solicitar al usuario el nombre del destinatario y el del remitente,
 # reemplazar esos marcadores en el texto, y mostrar la invitación personalizada por
 # pantalla.
+
+def personalizacion(file, destinatario, remitente):
+    try:
+        with open(file, "r+", encoding="utf-8") as archivo:
+            contenido = archivo.read()
+            contenido = contenido.replace("[DESTINATARIO]", destinatario)
+            contenido = contenido.replace("[REMITENTE]", remitente)
+        return contenido
+    except FileNotFoundError:
+        return "El archivo no existe."
+
+nombre_dest = input("Ingrese el nombre del destinatario: ")
+nombre_rem = input("Ingrese el nombre del remitente: ")
+archivo = input("Ingrese el nombre del archivo a modificar: ")
+
+print(personalizacion(archivo, nombre_dest, nombre_rem))
--- practica06-23/ejercicio12.py
+++ practica06-23/ejercicio12.py
@@ -2,3 +2,18 @@
 # codificado como una cadena binaria. Cada 8 bits representan un carácter
 # codificado en UTF-8. Escribir un programa en Python que muestre el mensaje
 # original por pantalla.
+
+try:
+    with open("mensaje.bin", "rb") as archivo:
+        contenido = archivo.read()
+    print(contenido)
+    mensaje = ""
+    for bit in range(0, len(contenido), 8):
+        byte = contenido[bit:bit+8]
+        letra = chr(int(byte, 2))
+        mensaje += letra
+    print(mensaje)
+except FileNotFoundError:
+    print("archivo inexistente.")
+except UnicodeError:
+    print("error al decodificar")
