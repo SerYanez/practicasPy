d87c4ee0db8badcdb58dd0ca8e52ab94ecde42fe
SerYanez <sergioyanez@outlook.com.ar>
2025-06-24 09:48:09 -0300

    Práctica de manejo de archivos

--- practica06-23/ejercicio13.py
+++ practica06-23/ejercicio13.py
@@ -4,3 +4,17 @@
 # - Reemplace las comas por tabulaciones (\t),
 # - Cambie los puntos decimales por comas,
 # - Muestre el contenido resultante con los nombres de las columnas.
+
+try:
+    with open("calidad_de_agua.txt", "r+") as archivo:
+        contenido = archivo.read()
+        contenido = contenido.replace(",", "\t\t\t")
+        contenido = contenido.replace(" ", "\t")
+        contenido = contenido.replace(".", ",")
+    columnas = ["Fecha", "Hora", "Caudal (m³/s)", "Temperatura promedio (°C)"]
+    for elemento in columnas:
+        print(elemento, end="\t\t")
+    print()
+    print(contenido)
+except FileNotFoundError:
+    print("Archivo no encontrado.")
